0d4d84244edea3878a9564bd22f82786a3432d0a
Your Name <you@example.com>
2022-06-25 16:57:09 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,10 @@ module.exports = {
     content: ['./*.html'],
     theme: {
         screens: {
-            'sm': '640px',
-            // => @media (min-width: 640px) { ... }
-
-            'md': '768px',
-            // => @media (min-width: 768px) { ... }
-
-            'lg': '1024px',
-            // => @media (min-width: 1024px) { ... }
-
-            'xl': '1280px',
-            // => @media (min-width: 1280px) { ... }
-
-            '2xl': '1536px',
-            // => @media (min-width: 1536px) { ... }
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
         },
         extend: {
             colors: {
