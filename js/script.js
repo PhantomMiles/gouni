1172caa63f3bdedd0298db246878a75cec464c53
Your Name <you@example.com>
2022-07-07 13:06:03 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,28 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
 });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() { myFunction() };
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        header.style.backgroundColor = "#EEE";
+        // header.style.marginTop = "10px";
+        header.style.transition = "ease-in 0.2s";
+
+    } else {
+        header.classList.remove("sticky");
+        header.style.backgroundColor = "transparent";
+
+    }
+}
