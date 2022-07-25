f58f859cdbfd8c5cd4671c221a5a358e73334a14
Your Name <you@example.com>
2022-07-25 16:57:01 +0100

    first commit

---
+++ js/script2.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open')
+    nav.classList.toggle('flex')
+    nav.classList.toggle('hidden')
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() { myFunction() };
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        header.style.backgroundColor = "#191970";
+        // header.style.marginTop = "10px";
+        header.style.transition = "ease-in 0.2s";
+
+    } else {
+        header.classList.remove("sticky");
+        header.style.backgroundColor = "#0F52BA";
+    }
+}
---
+++ login.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="icon" type="img/png" href="img/logo2.png">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="max-w-full relative container mx-auto py-4 md:py-4 px-3 bg-sapphireBlue">
+        <div class="flex items-center justify-between">
+            <!-- Logo -->
+            <div class="mx-auto">
+                <img src="img/logo2.png" class="h-10 md:h-20" alt="" />
+            </div>
+        </div>
+    </div>
+
+    <section id="event" class="bg-white">
+        <h1 class="text-4xl md:text-5xl text-center py-6 px-8 font-bold text-sapphireBlue">LOG IN</h1>
+        <!-- Flex Container -->
+        <div class="container flex flex-col items-center justify-between px-6 py-4 mx-auto space-y-12 md:py-4 md:space-y-0 bg-gray">
+            <form id="lgin-frm" action="account2.php" method="post" class="">
+                <div class="container flex flex-col mb-2 space-y-6">
+                    <div class="flex-col items-center justify-between px-6 py-4 mx-auto space-y-2">
+                        <label for="" class="block font-bold mx-40">Username</label>
+                        <input type="email"  name="username" id="" placeholder="Username">
+                    </div>
+                    <div class="flex-col items-center justify-between px-6 py-4 mx-auto space-y-2">
+                    <label for="" class="block font-bold">Password</label>
+                        <input type="password" name="password" id="" placeholder="Password">
+                    </div>
+                    <div class="flex justify-center">
+                        <button id="lgin" class="p-4 px-16 text-white bg-royalBlue rounded-lg baseline hover:bg-lightSkyBlue sm:hover-none focus:none outline:none text-md text-left font-bold md:mx-auto md:block">Log In</button>
+                    </div>
+                </div>      
+                <a href="#" id="cp_link" class="text-sapphireBlue text-center mx-60">Forgot your password?</a>
+            </form>
+        </div>
+    </section>
+    <footer class="bg-veryDarkBlue">
+        <!-- Flex container -->
+        <div class="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
+            <!-- Logo and social links container -->
+            <div class="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
+                <div class="mx-auto my-6 text-center text-white md:hidden">Copyright &copy; Godfrey Okoye University 2022, All Rights Reserved</div>
+                <!-- Logo -->
+                <div>
+                    <img src="img/logo-white.png" alt="" class="h-20 md:mt-10">
+                </div>
+                <!-- Social lInks container -->
+                <div class="flex justify-center space-x-4">
+                    <!-- Link 1 -->
+                    <a href="www.facebook.com"><img src="img/icon-facebook.svg" alt="" class="h-8"></a>
+                    <!-- Link 2 -->
+                    <a href="www.youtube.com"><img src="img/icon-youtube.svg" alt="" class="h-8"></a>
+                    <!-- Link 3 -->
+                    <a href="www.twitter.com"><img src="img/icon-twitter.svg" alt="" class="h-8"></a>
+                    <!-- Link 4 -->
+                    <a href="www.pinterest.com"><img src="img/icon-pinterest.svg" alt="" class="h-8"></a>
+                    <!-- Link 5 -->
+                    <a href="www.instagram.com"><img src="img/icon-instagram.svg" alt="" class="h-8"></a>
+                </div>
+            </div>
+            <!-- List container -->
+            <div class="flex justify-around space-x-32" id="nav2">
+                <div class="flex flex-col space-y-3 text-white">
+                    <a href="account.html" class="hover:text-skyBlue font-bold">Your Profile</a>
+                    <a href="index.html" class="hover:text-skyBlue font-bold">Home</a>
+                    <a href="events.html" class="hover:text-skyBlue font-bold">Events</a>
+                    <a href="news.html" class="hover:text-skyBlue font-bold">News</a>
+                    <a href="about.html" class="hover:text-skyBlue font-bold">About</a>
+                </div>
+                <div class="flex flex-col space-y-3 text-white">
+                    <a href="archive.html" class="hover:text-skyBlue font-bold">Archive</a>
+                    <a href="courses.html" class="hover:text-skyBlue font-bold">Courses</a>
+                    <a href="forum.html" class="hover:text-skyBlue font-bold">Forums</a>
+                    <a href="faq.html" class="hover:text-skyBlue font-bold">FAQs</a>
+                </div>
+            </div>
+            <!-- Input Container -->
+            <div class="flex flex-col justify-between">
+                <form action="">
+                    <div class="flex space-x-3">
+                        <input type="text" name="" id="" class="flex-1 w-1 px-4 rounded-full focus:outline-none" placeholder="Send us an email"> <button class="px-6 py-2 text-white rounded-full font-bold bg-royalBlue hover:bg-lightSkyBlue focus:outline:none">Send</button>
+                    </div>
+                </form>
+                <div class="hidden text-white md:block"> Contact us on the following lines:</div>
+                <div class="hidden text-white md:block md:mt-1/2">
+                    <ul id="tel" class="ml-5">
+                        <li>09134273215</li>
+                        <li>09121963049</li>
+                        <li>07061756730</li>
+                    </ul>
+                </div>
+                <div class="hidden text-white md:block">&copy; Godfrey Okoye University 2022, All Rights Reserved</div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/script2.js"></script>
+<!-- Code injected by live-server -->
+<script type="text/javascript">
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script></body>
+
+</html>
