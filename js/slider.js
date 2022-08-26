26cd6aa9d1ec63ade01ce25714847e6f9b3da7cf
Your Name <you@example.com>
2022-08-26 18:30:25 +0100

    first commit

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,30 @@
-var slider = document.querySelector("[data-slider] .top");
-function toggleSlider(params) {
-    for (let counter = 0; counter < slider.length; counter++) {
-        switch (counter) {
-            case 2:
-                slider.style.backgroundImage = "url(../images/slide1.png)";
-                slider.style.transition = "ease-in 0.5s";
-                break;
+// var slider = document.querySelector("[data-slider] .top");
+// function toggleSlider(params) {
+//     for (let counter = 0; counter < slider.length; counter++) {
+//         switch (counter) {
+//             case 2:
+//                 slider.style.backgroundImage = "url(../images/slide1.png)";
+//                 slider.style.transition = "ease-in 0.5s";
+//                 break;
         
-            default:
-                break;
-        }
+//             default:
+//                 break;
+//         }
         
-    }
-}
+//     }
+// }
+
+const sliders = [
+    "./img/homeslider.jpg",
+    "./img/slider1.jpg",
+    "./img/slider2.jpg",
+]
+
+const slide = document.getElementById('slide');
+
+setInterval(() => {
+    const randomPic = sliders[Math.floor(Math.random() * (sliders.length - 1))] 
+    console.log(randomPic)
+    slide.style.backgroundImage = `url(${randomPic})`;
+    slide.style.transition = "all 0.4s";
+}, 5000);
