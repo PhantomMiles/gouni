90ae5bc64ade2da8a5429494dd97aa3a973c809e
Your Name <you@example.com>
2022-08-31 16:45:55 +0100

    first commit

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,3 @@
-// var slider = document.querySelector("[data-slider] .top");
-// function toggleSlider(params) {
-//     for (let counter = 0; counter < slider.length; counter++) {
-//         switch (counter) {
-//             case 2:
-//                 slider.style.backgroundImage = "url(../images/slide1.png)";
-//                 slider.style.transition = "ease-in 0.5s";
-//                 break;
-        
-//             default:
-//                 break;
-//         }
-        
-//     }
-// }
-
 const sliders = [
     "./img/homeslider.jpg",
     "./img/slider1.jpg",
